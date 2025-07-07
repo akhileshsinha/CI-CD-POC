b6d2b4ed225e17de965043fad341e617a4ff28cd
Akhilesh Sinha <sinha.akhilesh@tftus.com>
2025-07-08 00:17:17 +0530

    testing CI/CD

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello World.
+          Hello World deployed on 8th July!
         </p>
        
       </header>
